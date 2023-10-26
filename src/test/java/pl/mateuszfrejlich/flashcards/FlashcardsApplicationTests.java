f0d22d82631adf7f129cc2ab4b7520f763c7f0d6
Mateusz Frejlich <mateuszfrejlich533@gmail.com>
2023-10-26 23:17:01 +0200

    add springboot project

---
+++ src/test/java/pl/mateuszfrejlich/flashcards/FlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mateuszfrejlich.flashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
