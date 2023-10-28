f1dfd31303888c8886a57f58396114fe136d8874
Mateusz Frejlich <mateuszfrejlich533@gmail.com>
2023-10-28 21:15:52 +0200

    configure swing

--- src/main/java/pl/mateuszfrejlich/flashcards/FlashcardsApplication.java
+++ src/main/java/pl/mateuszfrejlich/flashcards/FlashcardsApplication.java
@@ -1,13 +1,20 @@
 package pl.mateuszfrejlich.flashcards;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.swing.*;
 
 @SpringBootApplication
 public class FlashcardsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FlashcardsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(FlashcardsApplication.class).headless(false).web(WebApplicationType.NONE).run(args);
+        MainWindow window = ctx.getBean(MainWindow.class);
+        window.setTitle("Flashcards");
+        window.setSize(800, 600);
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
---
+++ src/main/java/pl/mateuszfrejlich/flashcards/MainWindow.java
@@ -0,0 +1,9 @@
+package pl.mateuszfrejlich.flashcards;
+
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+
+@Component
+public class MainWindow extends JFrame {
+}
