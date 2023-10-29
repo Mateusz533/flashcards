3431e602c16739e765a03385a88dc82db8f027db
Mateusz Frejlich <mateuszfrejlich533@gmail.com>
2023-10-29 17:40:03 +0100

    add raw layout

--- src/main/java/pl/mateuszfrejlich/flashcards/FlashcardsApplication.java
+++ src/main/java/pl/mateuszfrejlich/flashcards/FlashcardsApplication.java
@@ -5,16 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import javax.swing.*;
+import java.awt.*;
 
 @SpringBootApplication
 public class FlashcardsApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext ctx = new SpringApplicationBuilder(FlashcardsApplication.class).headless(false).web(WebApplicationType.NONE).run(args);
-        MainWindow window = ctx.getBean(MainWindow.class);
-        window.setTitle("Flashcards");
-        window.setSize(800, 600);
-        window.setVisible(true);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        EventQueue.invokeLater(() -> {
+            MainWindow window = ctx.getBean(MainWindow.class);
+        });
     }
 }
